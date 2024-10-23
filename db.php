8e749683d375791d9f19b196a05bdd7a308f301f
Grayyy1219 <lance.musngi@gmail.com>
2024-10-23 19:07:38 +0800

    dd

--- db.php
+++ db.php
@@ -1,12 +1,21 @@
 <?php
+// live
+
 $servername = "localhost";
 $username = "u297599468_guidancesrc";
 $password = "Guidancesrc2024";
 $dbname = "u297599468_guidancesrc_db";
+
+
+// local
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "miles";
 
 $con = new mysqli($servername, $username, $password, $dbname);
 
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
-?>
