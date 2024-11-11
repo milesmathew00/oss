1f9ab65b9885eee4e38989bd25da94682a7a67f6
milesmathew00 <padoxscapangpanganthree@gmail.com>
2024-11-12 01:53:48 +0800

    c

--- cumulative_records.php
+++ cumulative_records.php
@@ -257,8 +257,8 @@ mysqli_close($con);
                     <option value="3" <?php if ($filter_number_of_siblings == '3') echo 'selected'; ?>>3</option>
                     <option value="4" <?php if ($filter_number_of_siblings == '4') echo 'selected'; ?>>4</option>
                     <option value="5" <?php if ($filter_number_of_siblings == '5') echo 'selected'; ?>>5</option>
-                    <option value="5" <?php if ($filter_number_of_siblings == '6') echo 'selected'; ?>>6</option>
-                    <option value="5" <?php if ($filter_number_of_siblings == '7') echo 'selected'; ?>>7</option>
+                    <option value="6" <?php if ($filter_number_of_siblings == '6') echo 'selected'; ?>>6</option>
+                    <option value="7" <?php if ($filter_number_of_siblings == '7') echo 'selected'; ?>>7</option>
                     
 
                 </select>
